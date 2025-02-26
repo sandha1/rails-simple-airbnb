80dfaaaf4653cbe3f5ccecd19537b499296ebd24
Sandrine Halbronn sandhales@gmail.com <sandhales@gmail.com>
2025-02-26 16:53:24 +0100

    Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.destroy_all
+
+puts "Cleaned my flat database"
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cosy Appartment',
+  address: '12 Covent Street',
+  description: 'A small but charming appartment for a romantic gateway',
+  price_per_night: 120,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Big Loft',
+  address: '3 Luminous Street',
+  description: 'A spacious loft for big parties',
+  price_per_night: 60,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'One Flat',
+  address: '1 One Street',
+  description: 'A flat for the top',
+  price_per_night: 111,
+  number_of_guests: 1
+)
+
+puts "Created 4 new flats"
